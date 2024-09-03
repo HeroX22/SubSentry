55ef0f6d3f60a33bdbae60ff9d5c00ef4effee49
HeroX <hero.x.256@gmail.com>
2024-09-03 15:51:05 +0700

    update dikit

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ apt install git -y
 apt install wget -y
 
 #vul_detecition installation
-mkdir vuln_subsnipe
-cd vuln_subsnipe
 wget https://github.com/dub-flow/subsnipe/releases/download/v0.1.2/subsnipe-linux-amd64
 mv subsnipe-linux-amd64 subsnipe
 chmod +x subsnipe
@@ -15,4 +13,3 @@ git clone https://github.com/EdOverflow/can-i-take-over-xyz
 mv can-i-take-over-xyz fingerprints
 cd fingerprints
 mv fingerprints.json can-i-take-over-xyz_fingerprints.json
-cd ..
